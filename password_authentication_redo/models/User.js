ba36dfff32d691c1d4c753d7f5ada16b4179ed62
Sean Esteva <seangeleno@gmail.com>
2015-11-18 19:52:55 -0800

    added userSchema bcrypt basic stuff

--- password_authentication_redo/models/User.js
+++ password_authentication_redo/models/User.js
@@ -7,3 +7,16 @@ var userSchema = new Schema({
   local:    {name: String, email: String, password: String}
 , facebook: {id: String, name: String, token: String, email: String}
 })
+
+userSchema .methods.generateHash = function(passowrd){
+  return bcrypt.hashSynch(password, bcrypt.genSaltSynch(8),null)
+}
+
+userSchema.methods.validPassword = function(password){
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+var User = mongoose.model('User', userSchema)
+
+//last but not least we add the module.exports, can't communicate otherwise :S
+module.exports = User
