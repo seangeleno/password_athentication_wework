53f218148bfc90281952ee1d69667620c983e439
Sean Esteva <seangeleno@gmail.com>
2015-11-18 19:00:59 -0800

    added basic dependencies require on app.js

---
+++ password_authentication_redo/app.js
@@ -0,0 +1,12 @@
+var express      = require('express')
+,   app          = express()
+,   ejs          = require('ejs')
+,   ejsLayout    = require('express-ejs-layouts')
+,   mongoose     = require('mongoose')
+,   flash        = require('connect-flash')
+,   logger       = require('morgan')
+,   cookieParser = require('cookie-parser')
+,   bodyParser   = require('body-parser')
+,   session      = require('express-session')
+,   passport     = require('passport')
+,   port         = process.env.PORT || 3000
