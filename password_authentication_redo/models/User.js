9fd7364256b8600cbf6343f7945352ce0a1445a1
Sean Esteva <seangeleno@gmail.com>
2015-11-18 19:48:13 -0800

    added var userSchema

--- password_authentication_redo/models/User.js
+++ password_authentication_redo/models/User.js
@@ -1,3 +1,9 @@
 var mongoose = require('mongoose')
 ,   bcrypt   = require('bcrypt-nodejs')
 ,   Schema   = mongoose.Schema
+
+//write the userSchema now
+var userSchema = new Schema({
+  local:    {name: String, email: String, password: String}
+, facebook: {id: String, name: String, token: String, email: String}
+})
